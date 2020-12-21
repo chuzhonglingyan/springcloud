09d5d7c882215da758cfccd0097f781822f950da
yuntian <944610721@qq.com>
2020-12-22 01:05:24 +0800

    spring-cloud-stream学习

--- yuntian-cloud-stream-producer-9090/src/test/java/MessageProducerTest.java
+++ yuntian-cloud-stream-producer-9090/src/test/java/MessageProducerTest.java
@@ -1,13 +1,11 @@
 import com.yuntian.StreamProducerApplication9090;
 import com.yuntian.service.IMessageProducer;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest(classes = {StreamProducerApplication9090.class})
-@RunWith(SpringJUnit4ClassRunner.class)
 public class MessageProducerTest {
 
     @Autowired
